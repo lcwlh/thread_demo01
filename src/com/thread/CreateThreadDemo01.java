43a25134097863dbca3b368464fb028ef201974f
Hale <77582283@qq.com>
2019-05-09 11:02:16 +0800

    第一次

---
+++ src/com/thread/CreateThreadDemo01.java
@@ -0,0 +1,13 @@
+package com.thread;
+
+public class CreateThreadDemo01 extends Thread{
+    /**
+     * run方法就是线程需要执行的任务或者方法的代码
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println("run,i:"+i);
+        }
+    }
+}
